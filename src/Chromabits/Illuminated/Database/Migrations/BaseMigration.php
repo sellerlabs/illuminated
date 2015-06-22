6422c1133307c403026f6d8a248965be5183294b
Eduardo Trujillo <ed@chromabits.com>
2015-06-22 18:25:53 -0400

    Add more helpers to the base migration

--- src/Chromabits/Illuminated/Database/Migrations/BaseMigration.php
+++ src/Chromabits/Illuminated/Database/Migrations/BaseMigration.php
@@ -2,6 +2,7 @@
 
 namespace Chromabits\Illuminated\Database\Migrations;
 
+use Illuminate\Database\Connection;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Builder;
 
@@ -19,11 +20,30 @@ class BaseMigration extends Migration
     protected $builder;
 
     /**
+     * Current database connection.
+     *
+     * @var Connection
+     */
+    protected $db;
+
+    /**
      * Construct an instance of a BaseMigration
      */
     public function __construct()
     {
-        $this->builder = app('db')->connection($this->connection)
-            ->getSchemaBuilder();
+        $this->db = app('db')->connection($this->connection);
+        $this->builder = $this->db->getSchemaBuilder();
+    }
+
+    /**
+     * Return whether or not the current database driver is SQLite.
+     *
+     * Useful for avoiding running queries that are not compatible with SQLite.
+     *
+     * @return bool
+     */
+    protected function isSqlite()
+    {
+        return ($this->db->getConfig('driver') == 'sqlite');
     }
 }
