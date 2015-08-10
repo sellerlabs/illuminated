88c28dd8817a11a8ab2c3f7149d51dfb739b90ba
Eduardo Trujillo <ed@chromabits.com>
2015-08-10 01:09:44 -0400

    Add SubscriberInterface

---
+++ src/Chromabits/Illuminated/Events/Interfaces/SubscriberInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Chromabits\Illuminated\Events\Interfaces;
+
+use Illuminate\Contracts\Events\Dispatcher;
+
+/**
+ * Interface SubscriberInterface
+ *
+ * This interface describes a class capable of listening to events and handling
+ * them.
+ *
+ * @author Eduardo Trujillo <ed@chromabits.com>
+ * @package Chromabits\Illuminated\Events\Interfaces
+ */
+interface SubscriberInterface
+{
+    /**
+     * Subscribe to events.
+     *
+     * @param Dispatcher $events
+     *
+     * @return mixed
+     */
+    public function subscribe(Dispatcher $events);
+}
